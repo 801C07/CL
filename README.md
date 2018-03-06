3f04cb6c4433af01a98c701f3fdb979c31655944
Ben Whitehead <bwhthd@Bens-MBP.domain>
2018-03-06 15:36:35 -0800

    CL - mutithreaded

---
+++ README.md
@@ -0,0 +1,9 @@
+README:
+
+Follow instructions on https://www.smddzcy.com/2016/01/tutorial-multi-threading-in-php7-pthreads/ to install the proper version of PHP and pthreads. Don't vere from these instructions, setting up pthreads on OSX is a bitch.
+
+Consider running the following if there is an issue with older versions of php already on your system:
+```brew remove --force --ignore-dependencies httpd
+brew remove --force --ignore-dependencies php70-xdebug php71-xdebug
+brew remove --force --ignore-dependencies php70-imagick php71-imagick
+brew remove --ignore-dependencies --force php70 php71 php72```
--- cl.php
+++ cl.php
@@ -1,7 +1,158 @@
 <?php
 
-require 'QueryPath/src/qp.php';
+class CLSearch extends Thread
+{
+	function __construct($city, $params)
+	{
+		$this->city   = $city;
+		$this->params = $params;
+	}
 
+	public function run()
+	{
+		// echo microtime(true).PHP_EOL;
+		$this->data = basic_search($this->city, $this->params);
+	}
+}
+
+function basic_search($city, $params)
+{
+	$response = get("https://$city.craigslist.org/search/cto?".http_build_query($params));
+
+	require 'QueryPath/src/qp.php';
+
+	$html = htmlqp($response)->find('.result-image');
+	$results = [];
+
+	foreach ($html as $anchor)
+	{
+		$url = $anchor->attr('href');
+		if(strpos($url, 'ttp'))
+		{
+			$results[] = ['url' => $url, 'image' => $anchor->attr('data-ids') ?? null, 'price' => $anchor->find('span')->text() ?? null];
+		}
+	}
+
+	return json_encode($results);
+}
+
+function main()
+{
+	// require 'cities.php';
+	$cities = ['portland','boise','seattle','spokane'];
+	// $queries = ['Subaru h6', 'Subaru ll bean', 'Subaru l.l. Bean', 'Subaru VDC', 'subaru'];
+	// $queries = ['Tundra Crewmax', 'Tundra 1794', 'Tundra LTD', 'Tundra platinum'];
+	$queries = ['vanagon', 'eurovan', 'euro van', 'westfalia', 'delica', 'hiace'];
+	// $queries = ['cx-5','pathfinder','outlander', 'jeep'];
+	// $queries = ['subaru'];
+	// $queries = ['golf', 'vw golf'];
+	// $queries = ['cr v', 'cr-v'];
+	// $queries = ['sprinter',];
+	$results = [];
+	$remote_ids = [];
+
+	$params = [
+		'max_price'=>10000,
+		'min_price'=>400,
+		'max_auto_miles'=>150000,
+	];
+
+	foreach ($cities as $city) 
+	{
+		foreach ($queries as $query) 
+		{
+			$remote_ids[] = "$city|$query";
+		}
+	}
+
+	foreach ($remote_ids as &$remote_id) 
+	{
+		list($city, $query) = explode('|',$remote_id);
+		$params['query'] = $query;
+
+		$remote_id = new CLSearch($city, $params);
+		$remote_id->start();
+	}
+
+	foreach ($remote_ids as $remote_id)
+	{
+		try{
+			$remote_id->join();
+			$json = json_decode($remote_id->data);
+
+			foreach ($json as $result) 
+			{
+				preg_match('#\/(\d+).html#', $result->url, $matches);
+				$results[$matches[1]] = $result;
+			}
+		}catch(RuntimeException $e)
+		{
+		}
+	}
+
+
+	echo print_html($queries[0], $results);
+}
+
+function print_html($title, $list)
+{
+	$html = '<meta charset=utf-8><title>'.$title.'</title><style></style><script src=https://code.jquery.com/jquery-3.2.1.min.js crossorigin=anonymous integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="></script><link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">';
+
+	$html .= '<style>
+
+	.center-img {
+		display: block;
+	    margin-left: auto;
+	    margin-right: auto;
+	    width: 50%;
+		
+	}
+    </style>';
+
+	foreach ($list as $id => $result)
+	{
+		preg_match('#\/ct.\/d\/(.+)\/\d+.html#', $result->url, $title);
+
+		$html .= '<h2 class="w3-center">'.$title[1].'  '.$result->price.'</h2><div class="w3-content w3-display-container w3-center"><a href="'.$result->url.'">';
+	
+		foreach (explode(',', $result->image) as $image) 
+		{
+			$html .= '<img class="myslides-'.$id.' center-img" src="https://images.craigslist.org/'.(explode(':', $image)[1] ?? '').'_600x450.jpg"/>';
+		}
+		
+		$html .= '</a><button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1,'.$id.')">&#10094;</button><button class="w3-button w3-black w3-display-right" onclick="plusDivs(1,'.$id.')">&#10095;</button></a></div>';
+	}
+
+	$html .= '
+		<script>
+			var ids = ['.implode(',',array_keys($list)).'];
+
+			console.log(ids);
+			slideIndex = {};
+			ids.forEach(function(id){
+				slideIndex[id] = 1;
+				showDivs(slideIndex[id], id);
+			});
+
+			function plusDivs(n, id) {
+			    showDivs(slideIndex[id] += n, id);
+			}
+
+			function showDivs(n, id) {
+			    var i;
+			    var x = document.getElementsByClassName("myslides-"+id.toString());
+			    console.log("myslides-"+id);
+			    if (n > x.length) {slideIndex[id] = 1}
+			    if (n < 1) {slideIndex[id] = x.length} ;
+			    for (i = 0; i < x.length; i++) {
+			        x[i].style.display = "none";
+			    }
+			    x[slideIndex[id]-1].style.display = "block";
+			}
+		</script>';
+
+	return $html;
+}
 
 function get($url)
 {
@@ -29,7 +180,6 @@ function get($url)
 	{
 		return $response;
 	}
-
 }
 
 
@@ -80,133 +230,6 @@ function get_params()
 	}
 
 	return $params;
-}
-
-
-function basic_search($city, $params)
-{
-	$response = get("https://$city.craigslist.org/search/cta?".http_build_query($params));
-	$html = htmlqp($response)->find('.result-image');
-	$results = [];
-
-	foreach ($html as $anchor)
-	{
-		$url = $anchor->attr('href');
-		if(strpos($url, 'ttp'))
-		{
-			$results[] = ['url' => $url, 'image' => $anchor->attr('data-ids') ?? null, 'price' => $anchor->find('span')->text() ?? null];
-		}
-	}
-
-	return $results;
-}
-
-function main()
-{
-	require 'cities.php';
-	// $cities = ['portland'];
-	// $queries = ['Subaru h6', 'Subaru ll bean', 'Subaru l.l. Bean', 'Subaru VDC', 'subaru'];
-	// $queries = ['subaru'];
-	// $queries = ['golf', 'vw golf'];
-	// $queries = ['cr v', 'cr-v'];
-	$queries = ['sprinter',];
-	$results = [];
-
-	foreach ($cities as $city) 
-	{
-		foreach ($queries as $query) 
-		{
-			$params = [
-				'query'             => $query,
-				'srchType'          => 'T',
-				'max_price'         => 7000,
-				'min_price'         => 400,
-				'min_auto_miles'    => 1000,
-				'max_auto_miles'    => 150000,
-				'auto_title_status' => 1,
-				// 'auto_fuel_type'    => 2, //diesel
-				// 'auto_cylinders' 	=> 4, //6cyclinder
-				// 'auto_drivetrain' 	=> 3, //awd
-
-			];
-			$i = 0;
-			foreach(basic_search($city, $params) as $result)
-			{	
-				$results[] = $result;
-			}
-		}
-	}
-
-	echo print_html($queries[0], dedupe($results));
-}
-
-function dedupe($list)
-{
-	foreach ($list as $result) {
-		preg_match('#\/(\d+).html#', $result['url'], $matches);
-		$results[$matches[1]] = $result;
-	}
-	return $results;
-}
-
-function print_html($title, $list)
-{
-	$html = '<meta charset=utf-8><title>'.$title.'</title><style></style><script src=https://code.jquery.com/jquery-3.2.1.min.js crossorigin=anonymous integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="></script><link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">';
-
-	$html .= '<style>
-
-	.center-img {
-		display: block;
-	    margin-left: auto;
-	    margin-right: auto;
-	    width: 50%;
-		
-	}
-    </style>';
-
-	foreach ($list as $id => $result)
-	{
-		preg_match('#\/ct.\/d\/(.+)\/\d+.html#', $result['url'], $title);
-
-		$html .= '<h2 class="w3-center">'.$title[1].'  '.$result['price'].'</h2><div class="w3-content w3-display-container w3-center"><a href="'.$result['url'].'">';
-		foreach (explode(',', $result['image']) as $image) 
-		{
-			$html .= '<img class="myslides-'.$id.' center-img" src="https://images.craigslist.org/'.explode(':', $image)[1].'_600x450.jpg"/>';
-		}
-
-		
-		$html .= '</a><button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1,'.$id.')">&#10094;</button><button class="w3-button w3-black w3-display-right" onclick="plusDivs(1,'.$id.')">&#10095;</button></a></div>';
-	}
-
-	$html .= '
-		<script>
-			var ids = ['.implode(',',array_keys($list)).'];
-
-			console.log(ids);
-			slideIndex = {};
-			ids.forEach(function(id){
-				slideIndex[id] = 1;
-				showDivs(slideIndex[id], id);
-			});
-
-			function plusDivs(n, id) {
-			    showDivs(slideIndex[id] += n, id);
-			}
-
-			function showDivs(n, id) {
-			    var i;
-			    var x = document.getElementsByClassName("myslides-"+id.toString());
-			    console.log("myslides-"+id);
-			    if (n > x.length) {slideIndex[id] = 1}
-			    if (n < 1) {slideIndex[id] = x.length} ;
-			    for (i = 0; i < x.length; i++) {
-			        x[i].style.display = "none";
-			    }
-			    x[slideIndex[id]-1].style.display = "block";
-			}
-		</script>';
-
-	return $html;
 }
 
 
